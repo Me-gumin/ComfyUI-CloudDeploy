bf0cd24347fcb76c9ebf38bcfe4db210ec9beebb
Me-gumin <100194570+Me-gumin@users.noreply.github.com>
2025-09-02 20:40:04 +0800

    Update start.py
    修改了网页访问网址

--- start.py
+++ start.py
@@ -35,7 +35,7 @@ class ComfyUIService:
             
             # 启动localtunnel
             process = subprocess.Popen(
-                ["lt", "--subdomain", "lvy2025c_comfyui", "--port", str(self.port)],
+                ["lt", "--subdomain", "lvy2ccomfyui", "--port", str(self.port)],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 text=True
